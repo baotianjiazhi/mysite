08fc822c47600673681b0d3778e7a5f6effdbbc5
bao <524502256@qq.com>
2018-08-09 14:43:30 +0800

    博客巨幕修改

--- comment/admin.py
+++ comment/admin.py
@@ -5,3 +5,4 @@ from .models import Comment
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'content_object', 'text', 'comment_time', 'user', 'root', 'parent')
+
